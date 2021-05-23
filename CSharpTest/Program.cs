d54d2ce9e4f4898e968a8462ec9ab5d436830c00
jingbo <jingbo@nolovr.com>
2021-05-23 16:29:03 +0800

    init

---
+++ CSharpTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallUsingStringAsReturnValue();
+
+            Console.ReadKey();
+        }
+
+        [DllImport("CPPTEST", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string StringReturnAPI01();
+
+        static void CallUsingStringAsReturnValue()
+        {
+            string strReturn01 = StringReturnAPI01();
+            Console.WriteLine("Returned string : " + strReturn01);
+        }
+
+
+        [DllImport("CPPTEST")]
+        public static extern int fnCPPTest2();
+    }
+}
